b6ba4266a43fedb1c38bf72efe608d1137aa5fbd
Shlok Sinha <shlokjp@gmail.com>
2023-02-11 21:59:20 +0530

    nav lite for now

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,16 @@
 import React from "react";
-import About from "./About";
 
 export default function Navbar() {
   return (
     <nav>
       <h2 className="nav-title">Shlok Sinha</h2>
       <div className="nav-links">
-        <a href={<About />} className="nav-link-item">
-          About me
+        {/* <a className="nav-link-item">
+          Home
         </a>
-        <a href={<About />} className="nav-link-item">
-          Skills
-        </a>
-        <a href={<About />} className="nav-link-item">
-          Projects
-        </a>
-        <a href={<About />} className="nav-link-item">
+        <a  className="nav-link-item">
           Contact
-        </a>
+        </a> */}
       </div>
     </nav>
   );
