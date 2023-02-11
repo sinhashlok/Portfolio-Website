20ba4610f3679bbf1d7c91ca5664dba042292cb5
Shlok Sinha <shlokjp@gmail.com>
2023-02-11 11:24:12 +0530

    updated fonts

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <h2 className="nav-title">Shlok Sinha</h2>
+      <h2 className="nav-title">
+        Shlok <span style={{ color: "black" }}>Sinha</span>
+      </h2>
       <div className="nav-links">
         <p className="nav-link-item">About me</p>
         <p className="nav-link-item">Contact</p>
